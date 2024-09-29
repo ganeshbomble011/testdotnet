6d5754a946de85679e7ad39770f3005665c4b9ed
ganes <ganes@DESKTOP-VJ5P1HT>
2024-09-29 13:07:06 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication13.Models;
+
+
+namespace WebApplication13.Controllers
+{
+    public class HomeController : Controller
+    {
+        linqEntities s1 = new linqEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var s = s1.emps.ToList();
+            return View(s);
+        }
+        public ActionResult Admin()
+        {
+           return View();   
+        }
+        public ActionResult Register()
+        {
+            return View();  
+        }
+
+
+        public ActionResult Delete()
+        {
+            var x= s1.emps.Find(id);
+            s1.emps.Remove(x);  
+            RedirectToAction("Index", "Home");
+        }
+        
+
+
+
+    }
+}
